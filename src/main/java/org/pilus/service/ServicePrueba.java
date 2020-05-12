70dafbddd5fad226171ca9b694cd72e4274c6161
pilarledesmagonzalez <pilar.gonzalez@globallogic.com>
2020-05-12 20:12:09 -0300

    Correccion de error en conexion a DB. Actualizacion de POM.

--- src/main/java/org/pilus/service/ServicePrueba.java
+++ src/main/java/org/pilus/service/ServicePrueba.java
@@ -1,6 +1,9 @@
 package org.pilus.service;
 
 import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import org.pilus.pojos.PruebaPOJO;
 import java.lang.*;
 import java.net.UnknownHostException;
@@ -11,12 +14,16 @@ public class ServicePrueba {
 
     public static void connection() throws UnknownHostException {
 
-        MongoClient mongoClient = new MongoClient();
-        DB database = mongoClient.getDB("test");
-        DBCollection collection = database.getCollection("listingsAndReviews");
-        DBObject query = new BasicDBObject("_id", "10006546");
-        DBCursor cursor = collection.find(query);
-        System.out.println(cursor.toString());
+
+        MongoClient mongoClient = MongoClients.create(
+                "mongodb+srv://admin:admin@cluster0-66t9k.mongodb.net/test?retryWrites=true&w=majority");
+        MongoDatabase database = mongoClient.getDatabase("sample_airbnb");
+
+        System.out.println("Esto muestra la conexion al cluster, a una replica puntual, usando el puerto 27017");
+        System.out.println(database.listCollections().cursor().getServerAddress());
+
+
+        System.out.println("connected");
 
 
     }
