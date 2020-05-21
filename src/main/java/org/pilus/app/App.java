94ac222e52003d2f3a35c5a9a65c954d2dad6e48
pilarledesmagonzalez <pilar.gonzalez@globallogic.com>
2020-05-21 14:37:55 -0300

    Se crea clase DBConnection para connection strings y settings. Se eliminan imports innecesarios.

--- src/main/java/org/pilus/app/App.java
+++ src/main/java/org/pilus/app/App.java
@@ -2,20 +2,12 @@ package org.pilus.app;
 
 import org.pilus.controller.ControllerPrueba;
 import org.pilus.pojos.PruebaPOJO;
-import java.net.UnknownHostException;
-import static org.pilus.service.ServicePrueba.connection;
 
 public class App
 {
     public static PruebaPOJO prueba = PruebaPOJO.builder().name("pilus").build();
 
-    public static void main( String[] args ) throws UnknownHostException {
-        System.out.println(prueba.getId());
-        System.out.println(prueba.getName());
-        new ControllerPrueba().getInfo(prueba);
-        System.out.println(prueba.getId());
-        System.out.println(prueba.getName());
-        connection();
-
+    public static void main( String[] args ){
+        new ControllerPrueba().connection();
     }
 }
--- src/main/java/org/pilus/service/ServicePrueba.java
+++ src/main/java/org/pilus/service/ServicePrueba.java
@@ -1,64 +1,17 @@
 package org.pilus.service;
 
-import com.mongodb.*;
 import com.mongodb.client.*;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.model.Filters;
-import org.bson.BSONObject;
 import org.bson.Document;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
-import org.bson.types.ObjectId;
-import org.pilus.mapper.PruebaMapper;
 import org.pilus.pojos.PruebaPOJO;
 import java.lang.*;
-import java.net.UnknownHostException;
-import java.util.Objects;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-import org.bson.types.ObjectId;
 import static com.mongodb.client.model.Filters.eq;
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
-
 
 public class ServicePrueba {
 
-
-
-    public static void connection() throws UnknownHostException {
-
-        ConnectionString connectionString = new ConnectionString("mongodb+srv://admin:admin@cluster0-66t9k.mongodb.net/sample_airbnb?retryWrites=true&w=majority");
-
-        // Instancias codec para que haga la traduccion de BSON a POJO
-
-        CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
-
-        CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
-                pojoCodecRegistry);
-
-        MongoClientSettings clientSettings = MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .codecRegistry(codecRegistry)
-                .build();
-
-        //Conexion a la base
-        //MongoClient mongoClient = MongoClients.create(
-        //        "mongodb+srv://admin:admin@cluster0-66t9k.mongodb.net/sample_airbnb?retryWrites=true&w=majority");
-
-        MongoClient mongoClient = MongoClients.create(clientSettings);
-
-        MongoDatabase database = mongoClient.getDatabase("sample_airbnb");
-       // MongoDatabase database = mongoClient.getDatabase("sample_airbnb").withCodecRegistry(pojoCodecRegistry);
-
-
-        MongoCollection<PruebaPOJO> collection = database.getCollection("listingsAndReviews", PruebaPOJO.class);
-
-        PruebaPOJO review = collection.find(eq("_id", "10051164")).first();
+    public static void getByID() {
+        PruebaPOJO review = DBConnection.CollectionSelection().find(eq("_id", "10051164")).first();
         System.out.println(review);
-
-        mongoClient.close();
     }
 
     public static void readDocumentsFromDatabase(MongoCollection collection) {
@@ -73,10 +26,6 @@ public class ServicePrueba {
         Document myDoc = (Document) collection.find(eq("_id", id)).first();
         System.out.println(myDoc.toJson());
 
-    }
-
-    public static void getInfo(PruebaPOJO prueba){
-        prueba.setName("nombre cambiado");
     }
 
 }
